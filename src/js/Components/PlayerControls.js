c0de0dac67d7a349f305b34ded5bd6e75323da65
Tobias Dühr <nerd@tobiasduehr.com>
2020-01-11 22:59:39 +0100

    changed resolution

--- src/js/Components/PlayerControls.js
+++ src/js/Components/PlayerControls.js
@@ -1,33 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Req from '../Request';
-import PlayerState from '../Models/PlayerState';
-import { observer } from 'mobx-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import Req from "../Request";
+import PlayerState from "../Models/PlayerState";
+import { observer } from "mobx-react";
 
 @observer
-export default class PlayerControls extends React.Component{
-    componentDidMount(){
-        Req.send({ action: 'playlist' });
+export default class PlayerControls extends React.Component {
+    componentDidMount() {
+        Req.send({ action: "playlist" });
     }
-    render(){
-        if(PlayerState.state == 'stop') return null;
+    render() {
+        if (PlayerState.state == "stop") return null;
         let meta;
-        if(PlayerState.metadata){
-            meta = [
-                <span key="0" className="album">{ PlayerState.metadata.album }</span>,
-                <span key="1" className="title">{ PlayerState.metadata.title }</span>
-            ];
+        if (PlayerState.metadata) {
+            meta = (
+                <div className="metadata">
+                    <span key="0" className="album">
+                        {PlayerState.metadata.album}
+                    </span>
+                    <span key="1" className="title">
+                        {PlayerState.metadata.title}
+                    </span>
+                </div>
+            );
         }
         let playPause;
-        if(PlayerState.state == 'play') playPause = <button onClick={ PlayerState.pause }><i className="fa fa-pause" aria-hidden="true" /></button>;
-        if(PlayerState.state == 'pause') playPause = <button onClick={ PlayerState.play }><i className="fa fa-play" aria-hidden="true" /></button>;
-        return <div className="component-playercontrols">
-            <span className="num">{ (PlayerState.index + 1) + '/' + PlayerState.playlist.length }</span>
-            <div className="controls">
-                { playPause }
-                <button onClick={ PlayerState.next }><i className="fa fa-step-forward" aria-hidden="true" /></button>
+        if (PlayerState.state == "play")
+            playPause = (
+                <button onClick={PlayerState.pause}>
+                    <i className="fa fa-pause" aria-hidden="true" />
+                </button>
+            );
+        if (PlayerState.state == "pause")
+            playPause = (
+                <button onClick={PlayerState.play}>
+                    <i className="fa fa-play" aria-hidden="true" />
+                </button>
+            );
+        return (
+            <div className="component-playercontrols">
+                <div className="num">
+                    {PlayerState.index + 1 + "/" + PlayerState.playlist.length}
+                </div>
+                {meta}
+                <div className="controls">
+                    {playPause}
+                    <button onClick={PlayerState.next}>
+                        <i className="fa fa-step-forward" aria-hidden="true" />
+                    </button>
+                </div>
             </div>
-            { meta }
-        </div>
+        );
     }
 }
